70e378a5dac349117cdf66a67071591a310d1f9c
Jevon Cochran <jevon@securebags.com>
2023-08-04 18:43:38 -0500

    add social icons to nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
+import { SocialIcon } from "react-social-icons";
 
 interface NavRoute {
   name: string;
   href: string;
+}
+
+interface SocialLink {
+  url: string;
+  network: string;
+  bgColor?: string;
 }
 
 const routes: NavRoute[] = [
@@ -13,6 +20,26 @@ const routes: NavRoute[] = [
   { name: "Videos", href: "/videos" },
   { name: "Blog", href: "/blog" },
   { name: "Contact", href: "/contact" },
+];
+
+const socials: SocialLink[] = [
+  {
+    url: "https://instagram.com/blackdiasporer?igshid=MmIzYWVlNDQ5Yg==",
+    network: "instagram",
+  },
+  {
+    url: "https://www.facebook.com/profile.php?id=100070851596684",
+    network: "facebook",
+  },
+  {
+    url: "https://www.youtube.com/channel/UC3xsE8Sd2KJs5Scb-ttms2w",
+    network: "youtube",
+  },
+  {
+    url: "https://www.tiktok.com/@blackdiasporer?_t=8eZPzg96L7V&_r=1",
+    network: "tiktok",
+    bgColor: "#FFF",
+  },
 ];
 
 const Navbar = () => {
@@ -32,23 +59,25 @@ const Navbar = () => {
         justifyContent="space-between"
         alignItems="center"
       >
+        <Link href="/">
+          <Box
+            position="relative"
+            width="180px"
+            height="84px"
+            display="flex"
+            justifyContent="flex-start"
+          >
+            <Image
+              fill
+              objectFit="contain"
+              alt="Black Diasporer logo"
+              src="/black_diasporer_logo.png"
+              style={{ left: 0 }}
+            />
+          </Box>
+        </Link>
         <Box
-          position="relative"
-          width="180px"
-          height="84px"
-          display="flex"
-          justifyContent="flex-start"
-        >
-          <Image
-            fill
-            objectFit="contain"
-            alt="Black Diasporer logo"
-            src="/black_diasporer_logo.png"
-            style={{ left: 0 }}
-          />
-        </Box>
-        <Box
-          width="45%"
+          width="30%"
           display="flex"
           justifyContent="space-between"
           alignItems="center"
@@ -57,6 +86,21 @@ const Navbar = () => {
             <Link key={route.name} href={route.href}>
               <Typography textTransform="uppercase">{route.name}</Typography>
             </Link>
+          ))}
+        </Box>
+        <Box
+          width="11%"
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          {socials.map((social) => (
+            <SocialIcon
+              url={social.url}
+              network={social.network}
+              bgColor={social.bgColor}
+              style={{ height: 30, width: 30 }}
+            />
           ))}
         </Box>
       </Box>
